482a2550d620f1d5a7005b3b4c7c89b134bbf3e1
yuhiaht <18520822@gm.uit.edu.vn>
2020-05-04 23:43:33 +0700

    so for this second version, I come up with a brilliant idea: just use eval() lol

--- src/Calculator.js
+++ src/Calculator.js
@@ -9,22 +9,18 @@ class Calculator extends Component  {
         super(props);
         this.state = {
             input: 0,
-            currentVal: 0,
-            preVal: 0,
             screen: "0",
             result: 0,
-            currentSign: "",
-            preSign: ""
         };
     }
     /* maxDigitWarning = () => {
         this.setState({
-            currentVal: "Digt Limit , 21 Digit Max Mate",
-            prevVal: this.state.currentVal
+            currentResult: "Digt Limit , 21 Digit Max Mate",
+            prevVal: this.state.currentResult
         });
         setTimeout(()=>
             this.setState({
-                currentVal: this.state.prevVal
+                currentResult: this.state.prevVal
             }), 1000
         );
     } */
@@ -37,8 +33,6 @@ class Calculator extends Component  {
             screen: x,
             input: y,
         })
-        
-        
     }
 
     handleOperators = (e) => {
@@ -50,101 +44,35 @@ class Calculator extends Component  {
             screen: y,
             
         })
-        switch(x){
-            case "+":
-                this.setState({
-                    currentSign: "plus",
-                    currentVal: parseInt(this.state.currentVal) + parseInt(this.state.input),
-                    input: 0,
-                })
-            break;
-            case "-":
-                this.setState({
-                    currentSign: "minus",
-                    currentVal: this.state.currentVal == 0 ? 
-                    parseInt(this.state.input):
-                    parseInt(this.state.currentVal) - parseInt(this.state.input),
-                    input: 0,
-                })
-            break;
-            case "*":
-                this.setState({
-                    currentSign: "multiply",
-                    currentVal: this.state.currentVal == 0 ? 
-                    1 * parseInt(this.state.input) : 
-                    parseInt(this.state.currentVal) * parseInt(this.state.input),
-                    input: 0,
-                })
-            break;
-            case "/":
-                this.setState({
-                    currentSign: "divide",
-                    currentVal: this.state.currentVal == 0 ? 
-                    parseInt(this.state.input) :
-                    parseInt(this.state.currentVal) / parseInt(this.state.input),
-                    input: 0,
-                })
-            break;
-            
-            default: 
-            break;
-        }
-            
-            
-            
-        
-        
         /* else clearAll */
-        
     }
 
     handleResult = () => {
-        switch(this.state.currentSign){
-            case "plus":
-                this.setState({
-                    result: parseInt(this.state.currentVal) + parseInt(this.state.input),
-                })
-            break;
-            case "minus":
-                this.setState({
-                    result: parseInt(this.state.currentVal) - parseInt(this.state.input),
-                })
-            break;
-            case "multiply":
-                this.setState({
-                    result: parseInt(this.state.currentVal) * parseInt(this.state.input),
-                })
-            break;
-            case "divide":
-                this.setState({
-                    result: this.state.input != 0 ? 
-                    parseInt(this.state.currentVal) / parseInt(this.state.input) :
-                    "Math Error"
-                })
-            break;
-            default:
-            break;
-        }
-        
-        
+      this.setState({
+          result: eval(this.state.screen)
+      })
     }
 
     handleClear = () => {
         this.setState({
-            screen: "0",
             input: 0,
+            currentResult: 0,
+            preResult: 0,
             currentVal: 0,
-            preVal: this.state.result,
-            result: 0
+            preVal: 0,
+            screen: "0",
+            result: 0,
+            currentSign: "",
+            preSign: ""
         })
     }
 
-    handlePreAns = () => {
+    /* handlePreAns = () => {
         this.setState({
-            screen: this.state.preVal,
-            currentVal: parseInt(this.state.preVal)
+            screen: this.state.preResult,
+            currentResult: parseInt(this.state.preResult)
         })
-    }
+    } */
 
     handleDecimal = () => {
 
